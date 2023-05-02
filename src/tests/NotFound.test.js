8167a0e9d53b12ebab0397eb5b785de8d791c343
Enzo Gennari <enzo.gennari02@gmail.com>
2023-05-02 17:34:17 -0300

    Concluindo requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente "NotFound"', () => {
+  it('verifica se é renderizada corretamente quando a página NotFound deve ser exibida', () => {
+    renderWithRouter(<NotFound />);
+
+    screen.getByRole('heading', { name: /page requested not found/i });
+    const pikachuGIF = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(pikachuGIF).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
